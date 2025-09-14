e86445bb9e659ba55b03bfe4a4cda8a36f207920
Hayden Bohlen <bohlenhayden@gmail.com>
2025-09-14 06:39:21 -0500

    Update base.nix with system configurations

--- modules/base.nix
+++ modules/base.nix
@@ -1,8 +1,42 @@
-{ config, pkgs, ... }:
+#./modules/system/base.nix
+{ pkgs,... }:
+
 {
-  environment.systemPackages = with pkgs; [ vim git curl ];
-  services.openssh.enable = true;
-  system.stateVersion = "25.05";
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "zephyrus-m16";
   networking.networkmanager.enable = true;
-  # Common system-wide config
+
+  # Timezone and locale
+  time.timeZone = "America/New_York"; # <-- ❗ Feel free to change this
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Sound with PipeWire
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+
+  # User account
+  users.users.hbohlen = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    shell = pkgs.zsh;
+  };
+
+  # Nix settings
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
+  # Automatic garbage collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
 }
