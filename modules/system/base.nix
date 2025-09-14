2394745a333886281a0dcbc5cfda74b31693c635
hbohlen <bohlenhayden@gmail.com>
2025-09-14 09:11:20 -0500

    Switch to GRUB bootloader for better EFI compatibility

--- modules/system/base.nix
+++ modules/system/base.nix
@@ -3,7 +3,9 @@
 
 {
   # Bootloader
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Networking
